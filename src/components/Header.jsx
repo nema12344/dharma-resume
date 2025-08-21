34dd11981c1ab2fbdddaeefb1808e94b27a4b536
nema1950 <nemaram.choudhary@codinwalk.com>
2025-08-21 22:38:45 +0530

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react'
+
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <header>
       <div className="container">
         <nav>
           <h1>Dharmraj Choudhary</h1>
-          <div className="nav-links">
-            <a href="#about">About</a>
-            <a href="#education">Education</a>
-            <a href="#experience">Experience</a>
-            <a href="#skills">Skills</a>
-            <a href="#contact">Contact</a>
+          <div className={`nav-links ${isMenuOpen ? 'nav-links-mobile' : ''}`}>
+            <a href="#about" onClick={() => setIsMenuOpen(false)}>About</a>
+            <a href="#education" onClick={() => setIsMenuOpen(false)}>Education</a>
+            <a href="#experience" onClick={() => setIsMenuOpen(false)}>Experience</a>
+            <a href="#skills" onClick={() => setIsMenuOpen(false)}>Skills</a>
+            <a href="#contact" onClick={() => setIsMenuOpen(false)}>Contact</a>
+          </div>
+          <div className="mobile-menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
         </nav>
       </div>
